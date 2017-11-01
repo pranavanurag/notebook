f9e6c26c3c8555e4df9ba8336be2311fba114665
anuragpranav <anuragpranav@gmail.com>
2017-11-01 18:43:30 +0530

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -4,59 +4,67 @@ using namespace std;
 
 #define INF 123456789
 
-vector <int> pq;
+struct pq {
+	vector <int> x;
+	int n;
 
-void max_heapify(int i) {
-	int n = pq.size() - 1;
-
-	int l = i << 1, r = l + 1, idx = i;
-	if (l <= n && pq[l] > pq[i])
-		idx = l;
-	if (r <= n && pq[r] > pq[idx])
-		idx = r;
-
-	if (idx != i) {
-		swap(pq[idx], pq[i]);
-		max_heapify(idx);
+	pq() {
+		n = 0;
 	}
-}
 
-int get_max() {
-	return pq[1];
-}
-
-void pop() {
-	int n = pq.size() - 1;
-	swap(pq[1], pq[n]);
-	pq.pop_back();
-	max_heapify(1);
-}
-
-void build_max_heap() {
-	int n = pq.size() - 1;
-
-	for (int i = n/2; i >= 1; i--)
-		max_heapify(i);
-}
-
-void heap_sort() {
-	while (pq.size() > 1) {
-		cout<<get_max()<<" ";
-		pop();
+	void show() {
+		for (int i = 1; i <= n; i++)
+			cout<<x[i]<<" ";
+		cout<<endl;
 	}
-	cout<<endl;
-}
+
+	void max_heapify(int i) {
+		int l = i << 1, r = l + 1, idx = i;
+		if (l <= n && x[l] > x[i])
+			idx = l;
+		if (r <= n && x[r] > x[idx])
+			idx = r;
+
+		if (idx != i) {
+			swap(x[idx], x[i]);
+			max_heapify(idx);
+		}
+	}
+
+	int get_max() {
+		return x[1];
+	}
+
+	void pop() {
+		swap(x[1], x[n]);
+		x.pop_back();
+		n--;
+		max_heapify(1);
+	}
+
+	void build(vector <int> a) {
+		n = a.size();
+		x.resize(n + 1);
+		for (int i = 0; i < a.size(); i++)
+			x[i + 1] = a[i];
+
+		for (int i = n/2; i >= 1; i--)
+			max_heapify(i);
+	}
+};
 
 int main() {
 	ios::sync_with_stdio(false);
 	
 	int n;
 	cin>>n;
-	pq.resize(n + 1);
-	for (int i = 1; i <= n; i++)
-		cin>>pq[i];
+	vector <int> a;
+	a.resize(n);
+	for (int i = 0; i < n; i++)
+		cin>>a[i];
 
-	build_max_heap();
-	heap_sort();
+	pq q;
+	q.build(a);
+	q.show();
 	return 0;
 }
